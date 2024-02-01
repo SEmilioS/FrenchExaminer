e6e7449197b43412c2100fd1006d67d246ab780f
SEmilioS <ekss.0511@gmail.com>
2024-01-31 23:57:24 -0600

    Deleted unnecessary model

--- FEWebApp/FEWebApp/Models/MultipleChoiceViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FEWebApp.Models
-{
-    public class MultipleChoiceViewModel
-    {
-        public string QuestionText { get; set; }
-        public List<string> Choices { get; set; }
-    }
-}
