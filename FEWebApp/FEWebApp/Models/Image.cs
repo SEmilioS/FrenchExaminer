85e2b7a48b551262fdc30c923ca2fd79e58d7a01
SEmilioS <ekss.0511@gmail.com>
2024-02-01 23:29:46 -0600

    Moddied ResultMC to highlight correct answers
    Added new Class image to contain Base64 images
    
    Added 13 images to DB

---
+++ FEWebApp/FEWebApp/Models/Image.cs
@@ -0,0 +1,10 @@
+﻿namespace FEWebApp.Models
+{
+    public class Image
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string ImageBase64 { get; set; }
+    }
+}
