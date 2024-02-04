b4d3374cf2ca5721a5e3e92ca5db77773650d710
SEmilioS <ekss.0511@gmail.com>
2024-02-03 18:42:51 -0600

    Changed number of questions for RcNations

--- FEWebApp/FEWebApp/Controllers/ExamenController.cs
+++ FEWebApp/FEWebApp/Controllers/ExamenController.cs
@@ -39,7 +39,7 @@ namespace FEWebApp.Controllers
         {
             if (!_repositorio.NationsCompleted)
             {
-                var randomQuestions = _repositorio.getQuestionsNation(5);
+                var randomQuestions = _repositorio.getQuestionsNation(8);
 
                 _repositorio.nations = null;
                 _repositorio.nations = randomQuestions;
